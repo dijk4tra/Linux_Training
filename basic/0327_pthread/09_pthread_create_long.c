02808497bb990d616fc6b686d71559873fb48b1b
dijk4tra <dijk4tra@gmail.com>
2026-03-29 16:29:13 +0800

    0329 commit

--- basic/0327_pthread/09_pthread_create_long.c
+++ basic/0327_pthread/09_pthread_create_long.c
@@ -2,9 +2,13 @@
 
 void *thread_func(void *arg){
     printf("I am son\n");
-    
+    /* * 知识点 1：将指针强制转换回数值 (Passing by Value)
+     * - 这里的 arg 并不是一个有效的内存地址，而是主线程直接传过来的数字 10。
+     * - 我们将 void * 强制转换为 long，从而直接拿到了这个“值”。
+     * - 注意：这里 tmp 是子线程栈上的局部变量，对其修改不会影响主线程。
+     */
     long tmp = (long)arg;
-    tmp = 30;
+    tmp = 30; // 这里的修改仅在子线程内部有效
     printf("tmp: %ld\n", tmp);
     
     return NULL;
@@ -14,10 +18,19 @@ void *thread_func(void *arg){
 int main(int argc, char *argv[]){                                  
     
     //特殊情况: long型
+    /* * 知识点 2：为什么是 long 型？
+     * - 在 64 位 Linux 系统中，指针占 8 字节，long 也占 8 字节。
+     * - 如果用 int（4字节），在强制转换时可能会触发编译器的警告或数据截断。
+     * - 使用 long 可以完美“填满”指针参数的位宽，确保数据完整。
+     */
     long num = 10;
     pthread_t thread_id;
-    //pthread_create的第四个参数可以将值传递给线程入口参数
-    //这里传参本质上要传8个字节(一个指针,在64位系统,一个指针是8字节),那就直接传大小为8字节的long即可
+    /* * 知识点 3：传值 vs 传址 (核心差异)
+     * - 传址 : 传的是 &num，子线程通过地址修改主线程的变量。
+     * - 传值 : 传的是 (void *)num，即将 10 这个数字强行解释为一个地址发过去。
+     * - 优点：子线程拥有了该数值的一个独立副本，不需要担心主线程变量生命周期的问题
+     * - 即使主线程销毁了 num，子线程手里的 10 依然存在。
+     */
     int ret = pthread_create(&thread_id, NULL, thread_func, (void *)num); //创建了子线程                                                                       
     THREAD_ERROR_CHECK(ret, "pthread_create");
     
@@ -25,7 +38,9 @@ int main(int argc, char *argv[]){
     printf("***num: %ld\n", num);
 
     sleep(5); //为了让主线程等待子线程执行完毕
-
+    /* * 知识点 4：验证独立性
+     * 由于这次是“传值”，子线程里对 tmp 的任何修改，都不会反馈到主线程的 num 上。
+     */
     printf("num: %ld\n", num);
     
     return 0;
--- basic/0327_pthread/17_pthread_exit.c
+++ basic/0327_pthread/17_pthread_exit.c
@@ -2,13 +2,25 @@
 
 void func(){
     printf("func\n");
-    pthread_exit(NULL);//会让线程提前执行结束，那么后面的代码不会执行
+    /* * 知识点 1：pthread_exit 函数 (线程的“紧急出口”)
+     * - 功能：终止调用它的线程。
+     * - 特点：无论在哪个函数（哪怕是深层嵌套的子函数）中调用，都会立即结束整个线程。
+     * - 参数 (void *retval)：这就是线程的退出状态码。主线程 join 时拿到的就是这个值。
+     * - 后果：该行代码之后的所有代码（包括本函数和调用者函数中的）统统不会执行。
+     */
+    pthread_exit(NULL);
+    // 由于 pthread_exit 已经终结了线程，这一行 printf 永远不会被执行
     printf("+++++\n");
 }
 
 void *thread_func(void *arg){
     printf("I am son\n");
     func();
+    /* * 知识点 3：级联终止
+     * 虽然 func() 只是 thread_func 调用的一个子函数，但因为 func 内部执行了 pthread_exit，
+     * 整个子线程在 func 内部就断掉了，程序流永远不会回到这里。
+     * 所以下面这一行printf也不会被执行。
+     */
     printf("func over int thread_func\n");
     return NULL;
 }
@@ -17,12 +29,15 @@ void *thread_func(void *arg){
 int main(int argc, char *argv[]){                                  
     
     pthread_t thread_id;
-    //pthread_create的第四个参数可以将值传递给线程入口参数
     int ret = pthread_create(&thread_id, NULL, thread_func, NULL); //创建了子线程                                                                       
     THREAD_ERROR_CHECK(ret, "pthread_create");
     
     printf("I am main\n");
     
+    /* * 知识点 4：join 依然有效
+     * 即使子线程是通过 pthread_exit 提前退出的，主线程的 pthread_join 依然能正常
+     * 捕获到它退出的信号，并回收其资源。
+     */
     ret = pthread_join(thread_id, NULL);
     THREAD_ERROR_CHECK(ret, "pthread_join");
     printf("over\n");
